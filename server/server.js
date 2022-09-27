745a055f34c78edeb8357221b471647440db7a70
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-27 18:26:07 -0400

    added all needed mutations

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth')
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
 
@@ -8,7 +9,8 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 const app = express();
