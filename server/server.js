a5b6103adb5483dbfe89799f21e5e228c0a88418
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-28 17:19:47 -0400

    added ability to launch live project cohesively

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { authMiddleware } = require('./utils/auth')
+const path = require('path')
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
 
@@ -22,6 +23,14 @@ const startApolloSever = async (typeDefs, resolvers) => {
   await server.start()
   server.applyMiddleware({ app })
 }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
